ce72bcfebf662d9f60cd355bb84a5406af98ab39
UtsavKumar126/ <guptautsavkumar126@gmail.com>
2024-01-11 21:55:39 +0530

    second

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,51 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-class App extends Component {
-    render() {
+const App=()=>{
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
+    const[name1,setName1]=useState("");
+    const[name2,setName2]=useState("");
+    const[relation,setRelation]=useState('');
+
+    const Relationship=['Siblings','Friends','Love','Affection','Marriage','Enemy'];
+
+    function calculateStatus(){
+        let s1=name1;
+        let s2=name2;
+
+        for(let i=0;i<s1.length;i++){
+            if(s2.indexOf(s1.charAt(i))>-1){
+               s2=s2.replace(s1.charAt(i),'');
+               s1=s1.replace(s1.charAt(i),'');
+            }
+        }
+        setRelation(Relationship[(s1.length+s2.length)%6]);
+        
     }
+    function changeValue(){
+
+        setName1("");
+        setName2('');
+        setRelation("");
+    }
+    return(
+        <div id="main">
+           {/* Do not remove the main div */}
+           <input id="input1" 
+           onChange={(e)=>setName1(e.target.value)} 
+           value={name1}
+           placeholder="Enter first name"/>
+           <input id="input2"
+           onChange={(e)=>setName2(e.target.value)} 
+           value={name2}
+           placeholder="Enter second name"/>
+           <button id="calculate_relationship" onClick={calculateStatus}>Calculate Relationship Future</button>
+           <button id="clear" onClick={changeValue}>Clear</button>
+           {
+            relation!=""?<h3>{relation}</h3>:""
+           }
+        </div>
+    )
 }
 
 
